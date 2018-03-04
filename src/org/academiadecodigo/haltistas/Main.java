004c90754fabb827555484bf9a7168cbe85a910c
Sidonio Camara <sidoniofcamara@gmail.com>
2018-03-04 18:10:42 +0000

    Added to Main: newGame && newController

--- src/org/academiadecodigo/haltistas/Main.java
+++ src/org/academiadecodigo/haltistas/Main.java
@@ -1,4 +1,13 @@
 package org.academiadecodigo.haltistas;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.init();
+
+        Controller controller = new Controller();
+        controller.init();
+    }
 }
