f46e731f20e34d542e247a96713b8de5e4b1d15d
Sidonio Camara <sidoniofcamara@gmail.com>
2018-03-04 18:57:44 +0000

    Added constructor to Cursor

--- src/org/academiadecodigo/haltistas/game/Cursor.java
+++ src/org/academiadecodigo/haltistas/game/Cursor.java
@@ -1,6 +1,28 @@
 package org.academiadecodigo.haltistas.game;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+
 public class Cursor {
+
+    private int row;
+    private int col;
+
+
+    public Cursor(int row, int col) {
+
+        this.row = row;
+        this.col = col;
+
+
+        //Todo: Set cursor initial position in newRectangle
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.setColor(Color.BLUE);
+        rectangle.fill();
+
+    }
 
 
 }
